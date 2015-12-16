bd56d946a20b64fc309aa2a0fbc686fbf7749483
andrewmacquarrie <andrew.macquarrie@gmail.com>
2015-12-16 13:40:52 +0000

    rename install stuff

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 @echo off
 
 set SRC=%~dp0\x64\Release\
-set DEST=C:\Program Files\omiPlayer\
-set EXE=omiPlayer.exe
+set DEST=C:\Program Files\omiPlayerOmni\
+set EXE=omiPlayerOmni.exe
 
 echo Installing %SRC% into %DEST%
 xcopy /Y /D "%SRC%*.dll" "%DEST%"
@@ -10,8 +10,8 @@ xcopy /Y /D "%SRC%*.exe" "%DEST%"
 
 SET st2Path=%DEST%%EXE%
 echo Register shell extension
-@reg add "HKEY_CLASSES_ROOT\*\shell\Open with omiPlayer"         /t REG_SZ        /v ""     /d "Open with omiPlayer" /f
-@reg add "HKEY_CLASSES_ROOT\*\shell\Open with omiPlayer"         /t REG_EXPAND_SZ /v "Icon" /d "%st2Path%,0"              /f
-@reg add "HKEY_CLASSES_ROOT\*\shell\Open with omiPlayer\command" /t REG_SZ        /v ""     /d "%st2Path% \"%%1\""        /f
+@reg add "HKEY_CLASSES_ROOT\*\shell\Open with omiPlayerOmni"         /t REG_SZ        /v ""     /d "Open with omiPlayerOmni" /f
+@reg add "HKEY_CLASSES_ROOT\*\shell\Open with omiPlayerOmni"         /t REG_EXPAND_SZ /v "Icon" /d "%st2Path%,0"              /f
+@reg add "HKEY_CLASSES_ROOT\*\shell\Open with omiPlayerOmni\command" /t REG_SZ        /v ""     /d "%st2Path% \"%%1\""        /f
 
 pause
